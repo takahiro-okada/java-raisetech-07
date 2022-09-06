c316d1ec4a5538ecaa7660b7d3fca0ca84a4a93f
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-09-07 06:59:17 +0900

    feat: POSTでリクエストが受け取ったときに、names/idのURLを生成してbodyのパラメーターとして"name successfully created"を返却する

--- src/main/java/rest/api/raisetech/controller/IndexController.java
+++ src/main/java/rest/api/raisetech/controller/IndexController.java
@@ -1,9 +1,15 @@
 package rest.api.raisetech.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import rest.api.raisetech.domain.CreateForm;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,4 +20,15 @@ public class IndexController {
     public List<String> getName(){
         return List.of("Okada_GET","Takahiro_GET");
     }
+
+    //    POST
+    @PostMapping("/names")
+    public ResponseEntity<String> createName(@RequestBody CreateForm form){
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+                .path("/names/id")
+                .build()
+                .toUri();
+        return ResponseEntity.created(url).body("name successfully created!!");
+    }
+
 }
---
+++ src/main/java/rest/api/raisetech/domain/CreateForm.java
@@ -0,0 +1,14 @@
+package rest.api.raisetech.domain;
+
+public class CreateForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
