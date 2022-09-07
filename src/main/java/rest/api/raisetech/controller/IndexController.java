cb294ba86a9775bbc1d9c48c044dbd26a081b659
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-09-07 10:09:34 +0900

    feat: GETでValidationの実装。Nullチェック、国コードを10~999の範囲でしか受け取れないように指定

--- src/main/java/rest/api/raisetech/controller/IndexController.java
+++ src/main/java/rest/api/raisetech/controller/IndexController.java
@@ -2,19 +2,22 @@ package rest.api.raisetech.controller;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import rest.api.raisetech.domain.CreateForm;
-import rest.api.raisetech.domain.DeleteName;
-import rest.api.raisetech.domain.UpdateName;
-import rest.api.raisetech.domain.User;
+import rest.api.raisetech.domain.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Validated
 public class IndexController {
 
     //    GET
@@ -51,13 +54,23 @@ public class IndexController {
     private static final String birthDateText = "誕生日は%s";
     private final AtomicLong counter = new AtomicLong();
 
-
+    //    Validationの実装
     @GetMapping("/user")
     public User getUserInfo(
             @RequestParam(value = "name", defaultValue = "猫") String name,
             @RequestParam(value = "birthdate",defaultValue = "20001212") String birthdate
     ) {
         return new User(counter.incrementAndGet(), String.format(template,name), String.format(birthDateText,birthdate));
+    }
+
+
+    //    Validationの実装
+    @GetMapping("/country")
+    public Country getCountry(
+            @Valid @NotNull @Min(10) @Max(999) @RequestParam(value = "countrycode") Integer countrycode,
+            @RequestParam(value = "countryname",defaultValue = "Japan") String countryName
+    ){
+        return new Country(countrycode,countryName);
     }
 
 }
