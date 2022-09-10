e69c63dc4228d9e6d671bf01f1b85ad615fa0329
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-09-10 16:03:31 +0900

    feat: フォーマット

--- src/main/java/rest/api/raisetech/controller/IndexController.java
+++ src/main/java/rest/api/raisetech/controller/IndexController.java
@@ -20,21 +20,19 @@ import java.util.concurrent.atomic.AtomicLong;
 @Validated
 public class IndexController {
 
-    //    GET
-    @GetMapping("/names")
-    public List<String> getName(){
-        return List.of("Okada_GET","Takahiro_GET");
-    }
+  //    GET
+  @GetMapping("/names")
+  public List<String> getName() {
+    return List.of("Okada_GET", "Takahiro_GET");
+  }
 
-    //    POST
-    @PostMapping("/names")
-    public ResponseEntity<String> createName(@RequestBody CreateForm form){
-        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
-                .path("/names/id")
-                .build()
-                .toUri();
-        return ResponseEntity.created(url).body("name successfully created!!");
-    }
+  //    POST
+  @PostMapping("/names")
+  public ResponseEntity<String> createName(@RequestBody CreateForm form) {
+    URI url = UriComponentsBuilder.fromUriString("http://localhost:8080").path("/names/id").build()
+        .toUri();
+    return ResponseEntity.created(url).body("name successfully created!!");
+  }
 
     //    PATCH
     @PatchMapping("/names/{id}")
@@ -42,35 +40,33 @@ public class IndexController {
         return ResponseEntity.ok(Map.of("message","name successfully updated!!"));
     }
 
-    //    DELETE
-    @DeleteMapping("names/{id}")
-    public ResponseEntity<Map<String,String>> deleteName(@PathVariable int id, @RequestBody
-    DeleteName names){
-        return ResponseEntity.ok(Map.of("message","name successfully deleted!"));
-    }
+  //    DELETE
+  @DeleteMapping("names/{id}")
+  public ResponseEntity<Map<String, String>> deleteName(@PathVariable int id,
+                                                        @RequestBody DeleteName names) {
+    return ResponseEntity.ok(Map.of("message", "name successfully deleted!"));
+  }
 
-    //    GETでクエリ文字列を受け取る
-    public static final String template = "吾輩は, %sである!";
-    private static final String birthDateText = "誕生日は%s";
-    private final AtomicLong counter = new AtomicLong();
+  //    GETでクエリ文字列を受け取る
+  public static final String template = "吾輩は, %sである!";
+  private static final String birthDateText = "誕生日は%s";
+  private final AtomicLong counter = new AtomicLong();
 
-    //    Validationの実装
-    @GetMapping("/user")
-    public User getUserInfo(
-            @RequestParam(value = "name", defaultValue = "猫") String name,
-            @RequestParam(value = "birthdate",defaultValue = "20001212") String birthdate
-    ) {
-        return new User(counter.incrementAndGet(), String.format(template,name), String.format(birthDateText,birthdate));
-    }
+  //    Validationの実装
+  @GetMapping("/user")
+  public User getUserInfo(@RequestParam(value = "name", defaultValue = "猫") String name,
+                          @RequestParam(value = "birthdate", defaultValue = "20001212")
+                          String birthdate) {
+    return new User(counter.incrementAndGet(), String.format(template, name),
+        String.format(birthDateText, birthdate));
+  }
 
-
-    //    Validationの実装
-    @GetMapping("/country")
-    public Country getCountry(
-            @Valid @NotNull @Min(10) @Max(999) @RequestParam(value = "countrycode") Integer countrycode,
-            @RequestParam(value = "countryname",defaultValue = "Japan") String countryName
-    ){
-        return new Country(countrycode,countryName);
-    }
+  //    Validationの実装
+  @GetMapping("/country")
+  public Country getCountry(
+      @Valid @NotNull @Min(10) @Max(999) @RequestParam(value = "countrycode") Integer countrycode,
+      @RequestParam(value = "countryname", defaultValue = "Japan") String countryName) {
+    return new Country(countrycode, countryName);
+  }
 
 }
