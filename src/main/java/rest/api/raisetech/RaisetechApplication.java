7616f0fdf84eb5dcf9d67197d4d8a28166749671
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-09-10 16:01:46 +0900

    feat: フォーマット

--- src/main/java/rest/api/raisetech/RaisetechApplication.java
+++ src/main/java/rest/api/raisetech/RaisetechApplication.java
@@ -3,11 +3,11 @@ package rest.api.raisetech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:MissingJavadocType")
 @SpringBootApplication
 public class RaisetechApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RaisetechApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RaisetechApplication.class, args);
+  }
 
 }
